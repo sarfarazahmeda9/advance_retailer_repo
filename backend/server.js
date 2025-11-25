c16d2181ec3eb124ba9b061676937ad242e17287
sarfaraz.ahmed.a9@gmail.com <sarfaraz.ahmed.a9@gmail.com>
2025-11-25 18:36:08 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,21 @@ import connectDB from "./config/db.js";
 // import productRoutes from "./routes/productRoutes.js";
 import brandRoutes from "./routes/brand.route.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 //const MONGO_URI = process.env.MONGO_URI;
 
+const __dirname = path.resolve();
+
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+ }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -24,6 +31,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/brands", brandRoutes);
 app.use("/api/products", productRoutes);
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist", "index.html"));
+});
+
+
 
 app.listen(PORT, () => {
     connectDB();
